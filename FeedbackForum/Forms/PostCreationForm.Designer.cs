5a975863e1ee87c46c852dbe83bd35ed9f756fd0
Boris Bossink <426602@student.fontys.nl>
2022-03-07 14:49:40 +0100

    Text fields now show up based on what Attributes the selected category has.

--- FeedbackForum/Forms/PostCreationForm.Designer.cs
+++ FeedbackForum/Forms/PostCreationForm.Designer.cs
@@ -32,28 +32,28 @@ namespace FeedbackForum
             this.tbxName = new System.Windows.Forms.TextBox();
             this.lblName = new System.Windows.Forms.Label();
             this.cmbCategory = new System.Windows.Forms.ComboBox();
-            this.lblDescription = new System.Windows.Forms.Label();
-            this.tbxDescription = new System.Windows.Forms.TextBox();
             this.btnPost = new System.Windows.Forms.Button();
-            this.tbxMoreText = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbxName
             // 
             this.tbxName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbxName.Location = new System.Drawing.Point(132, 33);
+            this.tbxName.Location = new System.Drawing.Point(419, 148);
+            this.tbxName.Margin = new System.Windows.Forms.Padding(6);
             this.tbxName.Name = "tbxName";
-            this.tbxName.Size = new System.Drawing.Size(371, 29);
+            this.tbxName.Size = new System.Drawing.Size(974, 50);
             this.tbxName.TabIndex = 0;
             // 
             // lblName
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblName.Location = new System.Drawing.Point(23, 36);
+            this.lblName.Location = new System.Drawing.Point(63, 151);
+            this.lblName.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(39, 21);
+            this.lblName.Size = new System.Drawing.Size(81, 45);
             this.lblName.TabIndex = 1;
             this.lblName.Text = "Titel";
             // 
@@ -61,73 +61,58 @@ namespace FeedbackForum
             // 
             this.cmbCategory.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.cmbCategory.FormattingEnabled = true;
-            this.cmbCategory.Location = new System.Drawing.Point(524, 33);
+            this.cmbCategory.Location = new System.Drawing.Point(973, 70);
+            this.cmbCategory.Margin = new System.Windows.Forms.Padding(6);
             this.cmbCategory.Name = "cmbCategory";
-            this.cmbCategory.Size = new System.Drawing.Size(228, 29);
+            this.cmbCategory.Size = new System.Drawing.Size(420, 53);
             this.cmbCategory.TabIndex = 2;
             this.cmbCategory.SelectedIndexChanged += new System.EventHandler(this.cmbCategory_SelectedIndexChanged);
-            // 
-            // lblDescription
-            // 
-            this.lblDescription.AutoSize = true;
-            this.lblDescription.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lblDescription.Location = new System.Drawing.Point(23, 93);
-            this.lblDescription.Name = "lblDescription";
-            this.lblDescription.Size = new System.Drawing.Size(91, 21);
-            this.lblDescription.TabIndex = 3;
-            this.lblDescription.Text = "Onderschift";
-            // 
-            // tbxDescription
-            // 
-            this.tbxDescription.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbxDescription.Location = new System.Drawing.Point(132, 90);
-            this.tbxDescription.Multiline = true;
-            this.tbxDescription.Name = "tbxDescription";
-            this.tbxDescription.Size = new System.Drawing.Size(620, 57);
-            this.tbxDescription.TabIndex = 4;
             // 
             // btnPost
             // 
             this.btnPost.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnPost.Location = new System.Drawing.Point(639, 378);
+            this.btnPost.Location = new System.Drawing.Point(1187, 806);
+            this.btnPost.Margin = new System.Windows.Forms.Padding(6);
             this.btnPost.Name = "btnPost";
-            this.btnPost.Size = new System.Drawing.Size(113, 40);
+            this.btnPost.Size = new System.Drawing.Size(210, 85);
             this.btnPost.TabIndex = 5;
             this.btnPost.Text = "Post!";
             this.btnPost.UseVisualStyleBackColor = true;
             this.btnPost.Click += new System.EventHandler(this.btnPost_Click);
             // 
-            // tbxMoreText
-            // 
-            this.tbxMoreText.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.tbxMoreText.Location = new System.Drawing.Point(132, 176);
-            this.tbxMoreText.Name = "tbxMoreText";
-            this.tbxMoreText.Size = new System.Drawing.Size(620, 29);
-            this.tbxMoreText.TabIndex = 6;
-            // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(23, 179);
+            this.label1.Location = new System.Drawing.Point(43, 382);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 21);
+            this.label1.Size = new System.Drawing.Size(0, 45);
             this.label1.TabIndex = 7;
-            this.label1.Text = "More Text";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCategory.Location = new System.Drawing.Point(63, 73);
+            this.lblCategory.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(158, 45);
+            this.lblCategory.TabIndex = 8;
+            this.lblCategory.Text = "Categorie";
             // 
             // PostCreationForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1486, 960);
+            this.Controls.Add(this.lblCategory);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.tbxMoreText);
             this.Controls.Add(this.btnPost);
-            this.Controls.Add(this.tbxDescription);
-            this.Controls.Add(this.lblDescription);
             this.Controls.Add(this.cmbCategory);
             this.Controls.Add(this.lblName);
             this.Controls.Add(this.tbxName);
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "PostCreationForm";
             this.Text = "Nieuwe Post";
             this.ResumeLayout(false);
@@ -140,11 +125,9 @@ namespace FeedbackForum
         private System.Windows.Forms.TextBox tbxName;
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.ComboBox cmbCategory;
-        private System.Windows.Forms.Label lblDescription;
-        private System.Windows.Forms.TextBox tbxDescription;
         private System.Windows.Forms.Button btnPost;
-        private System.Windows.Forms.TextBox tbxMoreText;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCategory;
     }
 }
 
--- FeedbackForum/Forms/PostCreationForm.cs
+++ FeedbackForum/Forms/PostCreationForm.cs
@@ -16,6 +16,7 @@ namespace FeedbackForum
         private PostContainer postContainer;
         private CategoryContainer categoryContainer;
         private Category selectedCategory;
+        private List<Control[]> allFields = new List<Control[]>();
         public PostCreationForm()
         {
             InitializeComponent();
@@ -30,12 +31,18 @@ namespace FeedbackForum
 
         private void btnPost_Click(object sender, EventArgs e)
         {
-            if (tbxName.TextLength <= 0 || tbxDescription.TextLength <= 0 || cmbCategory.SelectedIndex < 0 || tbxMoreText.TextLength <= 0)
+            if (tbxName.TextLength <= 0 || cmbCategory.SelectedIndex < 0)
                 return;
 
+            
             Post post = new Post(tbxName.Text, selectedCategory);
-            post.SetAttributeValue("Description", tbxDescription.Text);
-            post.SetAttributeValue("More Text", tbxMoreText.Text);
+            foreach (Control[] field in allFields)
+            {
+                if (field[1].Text.Length <= 0)
+                    return;
+
+                post.SetAttributeValue(field[0].Text, field[1].Text);
+            }
 
             postContainer.Add(post);
             MessageBox.Show("Created new Post! This post has: \nTitle: " + post.Name + "\nUpload Date: " + post.CreationDate + "\nCategory: " + post.Category.Name + "\nAttributes" + post.Category.Attributes.Keys);
@@ -48,8 +55,52 @@ namespace FeedbackForum
                 if (cmbCategory.SelectedItem.ToString() == category.Name)
                 {
                     selectedCategory = category;
+                    DeleteFields();
+                    ShowFields();
                     return;
                 }
+            }
+        }
+        private void DeleteFields()
+        {
+            foreach(Control[] field in allFields)
+            {
+                Controls.Remove(field[0]);
+                Controls.Remove(field[1]);
+            }
+        }
+        private void ShowFields()
+        {
+            int i = 90;
+            foreach (KeyValuePair<string,string> attribute in selectedCategory.Attributes)
+            {
+                Label label = new Label()
+                {
+                    AutoSize = true,
+                    Text = attribute.Key,
+                    Size = lblName.Size,
+                    Font = lblName.Font,
+                    Location = new Point(lblName.Location.X, lblName.Location.Y + i)
+                };
+
+                TextBox textbox = new TextBox()
+                {
+                    Size = tbxName.Size,
+                    Font = tbxName.Font,
+                    Location = new Point(tbxName.Location.X, tbxName.Location.Y + i)
+                };
+
+                Console.WriteLine(label.Text);
+
+                this.Controls.Add(label);
+                this.Controls.Add(textbox);
+
+                allFields.Add(new Control[2]
+                {
+                    label,
+                    textbox
+                });
+                i += 90;
             }
         }
     }
